7484acfb7a9ab1d9a9fa9bf601a7f245f78f6818
vaaski <admin@vaa.ski>
2016-09-05 00:54:04 +0200

    Themed Gmail; started to increase the build number per commit

--- BWAF/app/build/generated/source/buildConfig/debug/substratum/theme/template/BuildConfig.java
+++ BWAF/app/build/generated/source/buildConfig/debug/substratum/theme/template/BuildConfig.java
@@ -8,7 +8,7 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.vaaski.bwaf";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 1;
+  public static final int VERSION_CODE = 2;
   public static final String VERSION_NAME = "pre- release";
   // Fields from build type: debug
   public static final boolean ENABLE_ANTI_PIRACY = false;
